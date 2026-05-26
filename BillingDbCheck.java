fbbeb1eeca459f341cb9d370f8705bb866ba3bf6
Sameer <siddheshvikram1@gmail.com>
2026-05-26 18:06:27 +0530

    Improve structure (2/5)

--- BillingDbCheck.java
+++ BillingDbCheck.java
@@ -4,3 +4,9 @@ public class BillingDbCheck {
     String url = "jdbc:mysql://localhost:3306/nayan-db?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
     try (Connection conn = DriverManager.getConnection(url, "root", "root")) {
       try (Statement st = conn.createStatement()) {
+        for (String table : new String[]{"billing_products", "billing_records"}) {
+          try (ResultSet rs = st.executeQuery("SELECT COUNT(*) FROM " + table)) {
+            rs.next();
+            System.out.println(table + " rows=" + rs.getInt(1));
+          }
+        }
