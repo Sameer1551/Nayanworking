a79223bb8259f1c6fc07fa2d9e650d825c38df4a
Sameer <siddheshvikram1@gmail.com>
2026-06-11 15:59:13 +0530

    Add functionality (4/5)

--- database-utilities/BillingFinalCheck.java
+++ database-utilities/BillingFinalCheck.java
@@ -7,3 +7,6 @@ public class BillingFinalCheck {
         try (ResultSet rs = st.executeQuery("SELECT COUNT(*) FROM billing_records")) { rs.next(); System.out.println("billing_records=" + rs.getInt(1)); }
         try (ResultSet rs = st.executeQuery("SELECT COUNT(*) FROM billing_products")) { rs.next(); System.out.println("billing_products=" + rs.getInt(1)); }
         try (ResultSet rs = st.executeQuery("SELECT AUTO_INCREMENT FROM information_schema.TABLES WHERE TABLE_SCHEMA='nayan-db' AND TABLE_NAME='billing_records'")) { if (rs.next()) System.out.println("billing_records next_id=" + rs.getLong(1)); }
+      }
+    }
+  }
