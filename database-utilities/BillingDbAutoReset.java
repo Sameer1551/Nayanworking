3b63474df55bf05d4fc0afcc14aa6258b52d7eca
Sameer <siddheshvikram1@gmail.com>
2026-06-11 15:35:59 +0530

    Code adjustments (2/5)

--- database-utilities/BillingDbAutoReset.java
+++ database-utilities/BillingDbAutoReset.java
@@ -2,3 +2,7 @@ import java.sql.*;
 public class BillingDbAutoReset {
   public static void main(String[] args) throws Exception {
     String url = "jdbc:mysql://localhost:3306/nayan-db?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
+    try (Connection conn = DriverManager.getConnection(url, "root", "root")) {
+      try (Statement st = conn.createStatement()) {
+        st.execute("ALTER TABLE billing_products AUTO_INCREMENT = 1");
+        st.execute("ALTER TABLE billing_records AUTO_INCREMENT = 1");
