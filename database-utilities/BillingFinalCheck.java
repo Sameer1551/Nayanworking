4b2078c2dcdc86b085f3b4c11019fb3027eaaecb
Sameer <siddheshvikram1@gmail.com>
2026-06-11 15:57:28 +0530

    Optimize implementation (2/5)

--- database-utilities/BillingFinalCheck.java
+++ database-utilities/BillingFinalCheck.java
@@ -1,3 +1,6 @@
 import java.sql.*;
 public class BillingFinalCheck {
   public static void main(String[] args) throws Exception {
+    String url = "jdbc:mysql://localhost:3306/nayan-db?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
+    try (Connection conn = DriverManager.getConnection(url, "root", "root")) {
+      try (Statement st = conn.createStatement()) {
