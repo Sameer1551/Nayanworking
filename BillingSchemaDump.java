4e9a2ec3e751e6cc9bf3d4eaec24c6e5b934f648
Sameer <siddheshvikram1@gmail.com>
2026-06-11 15:25:55 +0530

    Fix issue (2/5)

--- BillingSchemaDump.java
+++ BillingSchemaDump.java
@@ -2,3 +2,7 @@ import java.sql.*;
 public class BillingSchemaDump {
   public static void main(String[] args) throws Exception {
     String url = "jdbc:mysql://localhost:3306/nayan-db?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
+    try (Connection conn = DriverManager.getConnection(url, "root", "root")) {
+      try (Statement st = conn.createStatement()) {
+        for (String table : new String[]{"billing_products", "billing_records"}) {
+          try (ResultSet rs = st.executeQuery("SHOW CREATE TABLE " + table)) {
